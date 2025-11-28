c70510050619eed5bc994054c43c7149c0bc6f3b
makej7403-bit <makej7403@gmail.com>
2025-11-28 19:00:13 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.firebasestorage.googleapis.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.firebasestorage.app",
-      },
-    ],
+  webpack: (config) => {
+    config.externals.push({ undici: "commonjs undici" }); 
+    return config;
   },
 };
 
