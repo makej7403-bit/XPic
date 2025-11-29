c1e2d15ed5b7c27bf592b2a49be2d0b7421635db
makej7403-bit <makej7403@gmail.com>
2025-11-29 18:27:24 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,18 @@
+import "./globals.css";
+import Header from "@/components/Header";
+
 export const metadata = {
   title: "XPic",
-  description: "Upload and view pictures online",
+  description: "Photo sharing app for everyone",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-gray-100">
+        <Header />
+        <main className="mt-4">{children}</main>
+      </body>
     </html>
   );
 }
